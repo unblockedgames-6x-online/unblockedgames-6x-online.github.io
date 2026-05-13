c6f6f4c44378abbcdb71b231cefeaedc315ce1c8
unblockedgames-6x-online <ahhsnsnnss@gmail.com>
2026-05-13 11:38:36 +0500

    Update footer.js

--- assest/js/footer.js
+++ assest/js/footer.js
@@ -239,7 +239,11 @@ function removeGameStorage(me, e) {
         default:
     }
 }
-
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w8gd5xklsh");
 function likeGames(game_id) {
     if (!game_id) return;
     let url = '/likegame.ajax';
